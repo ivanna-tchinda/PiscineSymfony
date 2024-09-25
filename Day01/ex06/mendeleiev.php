40471b9bb001baa1346e783c08b476fc7bbac0d5
ivanna-tchinda <ivanna.tchinda@gmail.com>
2024-09-25 10:55:26 +0200

    ex06 ok

--- Day01/ex06/mendeleiev.php
+++ Day01/ex06/mendeleiev.php
@@ -10,7 +10,7 @@ $html_content = '<html>
         <title>Mendeleiev Table</title>
         <style>
             li{
-                font-size: 11px;
+                font-size: 10px;
             }
         </style>
     </head>
@@ -20,9 +20,29 @@ $html_content = '<html>
 file_put_contents($html_file, $html_content);
 
 function empty_cell($num){
-    if(($num > 0 && $num < 17) || ($num > 19 && $num < 30) || ($num > 37 && $num < 48) || $num == 56)
+    if(($num > 0 && $num < 17) || ($num > 19 && $num < 30) || ($num > 37 && $num < 48) || ($num == 92) || ($num == 110))
         return true;
     return false;
+}
+
+function getcolor($num){
+    if($num == 0 || ($num > 30 && $num < 34) || ($num > 49 && $num < 52) || ($num == 69))
+        return("green");
+    if($num == 18 || $num == 36 || $num == 54 || $num == 72 || $num == 90 || $num == 108)
+        return("red");
+    if($num == 19 || $num == 37 || $num == 55 || $num == 73 || $num == 91 || $num == 109)
+        return("#fcefc0");
+    if($num == 17 || $num == 35 || $num == 53 || $num == 71 || $num == 89 || $num == 107)
+        return("#c0f3fc");
+    if(($num > 55 && $num < 65) || ($num > 73 && $num < 83) || ($num > 92 && $num < 101) || ($num > 110 && $num < 116) || $num == 119)
+        return("#fcc0f3");
+    if(($num == 48) || ($num > 64 && $num < 67) || ($num > 82 && $num < 86) || ($num > 100 && $num < 106))
+        return("#cfcace");
+    if($num == 34 || $num == 52 || $num == 70 || $num == 88)
+        return("#fcffa1");
+    if($num == 30 || $num == 49 || ($num > 66 && $num < 69) || ($num > 85 && $num < 88) || $num == 106)
+        return("#9ba396");
+    return("white");
 }
 
 function tab_content(){
@@ -30,29 +50,36 @@ function tab_content(){
     $lines = explode("\n", $content);
     $tab = "";
     $element_index = 0;
+    $index = 0;
     for($row = 0; $row < 7; $row++){
         $tab .= "<tr style='border: solid; height: 100px'>";
         for($col = 0; $col < 18 && $element_index < 88; $col++){
-            if(empty_cell($element_index)){
-                $tab .= "<td style='border: solid;'>0</td>";
+            if(empty_cell($index)){
+                $tab .= "<td style='border: solid;'></td>";
             }
             else{
                 $infos = explode(",", $lines[$element_index]);
                 $first_arg = explode(" ", $infos[0]);
                 $name = $first_arg[0];
                 $position = $first_arg[2];
-                $tab .= "<td style='border: solid';>" . 
-                    "<h4>" . $name . "</h4>" .
-                    "<ul style='list-style-type:none;'>" .
+                $number = $infos[1];
+                $small = $infos[2];
+                $molar = $infos[3];
+                $electron = $infos[4];
+                $getcolor_cell = getcolor($index);
+                $tab .= "<td style='border: solid; background-color:$getcolor_cell'>" . 
+                    "<h4 style='text-align:center;'>" . $name . "</h4>" .
+                    "<ul style='list-style-type:none; margin: 0;'>" .
                         "<li>" . $position . "</li>" .
-                        "<li>" . $position . "</li>" .
-                        "<li>" . $position . "</li>" .
-                        "<li>" . $position . "</li>" .
-                        "<li>" . $position . "</li>" .
+                        "<li>" . $number . "</li>" .
+                        "<li>" . $small . "</li>" .
+                        "<li>" . $molar . "</li>" .
+                        "<li>" . $electron . "</li>" .
                     "</ul>" 
                 ."</td>";
+                $element_index++;
             }
-            $element_index++;
+            $index++;
         }
         $tab .= "</tr>";
     }
@@ -62,7 +89,7 @@ function tab_content(){
 function build_table(){
     $content = "<h4 style='text-align:center; margin-top: 50px;'>Tableau periodique des elements chimiques</h4>";
     $table_content = tab_content();
-    $content .= "<table style='width: 80%'>" . $table_content . "</table>";
+    $content .= "<table style='width: 70%'>" . $table_content . "</table>";
     return $content;
 }
 
