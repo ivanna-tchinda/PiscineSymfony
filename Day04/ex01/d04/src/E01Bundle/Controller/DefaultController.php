e218d074cf515035a97a0b74aff3621a19146206
ivanna <ivanna.tchinda@gmail.com>
2024-10-01 15:59:40 +0200

    ex01

--- Day04/ex01/d04/src/E01Bundle/Controller/DefaultController.php
+++ Day04/ex01/d04/src/E01Bundle/Controller/DefaultController.php
@@ -8,19 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/e01/', name: 'homepage')]
+    #[Route('/e01/', name: 'e01_homePage')]
     public function index(): Response
     {
-        $myName = 'Ivanna'; 
-        return $this->render('base.html.twig', [
-            'name' => $myName
-        ]);
+        return $this->render('homePage/index.html.twig');
     }
 
-    #[Route('/e01/{article}', name: 'e01_yes')]
+    #[Route('/e01/{article}', name: 'e01_article')]
     public function index2(string $article): Response
     {
-        return new Response("$article");
+        $articles = ["bluewhale", "goeland", "tiger"];
+        $view = "homePage/index.html.twig";
+
+        if(in_array($article, $articles)){
+            $view = "articles/$article/index.html.twig";
+        }
+        return $this->render($view);
     }
 }
 
