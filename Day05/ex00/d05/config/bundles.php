76fbb7d2e02ef03a1daaf0fe92ec88dfb81c56ad
itchinda <ivanna.tchinda@gmail.com>
2024-10-14 11:46:59 +0200

    ex00

--- Day05/ex00/d05/config/bundles.php
+++ Day05/ex00/d05/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     App\ex00\ex00::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- Day05/ex00/d05/src/ex00/Controller/DefaultController.php
+++ Day05/ex00/d05/src/ex00/Controller/DefaultController.php
@@ -10,18 +10,44 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/ex00', name: 'ex00')]
-    public function index()
+    #[Route('/ex00', name: 'home')]
+    public function index(Environment $twig, Request $request)
+    {
+        return new Response($twig->render('table/index.html.twig'));
+    }
+
+    #[Route('/create_table', name: 'create_table')]
+    public function createTable()
     {
         $db_server = "localhost";
         $db_user = $this->getParameter('db_user');
         $db_pass = $this->getParameter('db_pass');
+        $db_name = "ex00";
 
-        $connection = mysqli_connect($db_server, $db_user, $db_pass);
+        $connection = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
         if(!$connection){
             return new Response("connection failed" . mysqli_connect_error());
         }
+
+        $sql = "
+        CREATE TABLE IF NOT EXISTS Users (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(30) NOT NULL,
+            _name VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            _enable BOOLEAN,
+            birthdate DATETIME,
+            _address LONGTEXT
+            )";
+
+        try {
+            $connection->query($sql);
+            return new Response("Table Users created successfully");
+        } catch (Exception $e){
+            return new Response("Error creating table: $e");
+        }
+        $connection->close();
         return new Response("ex00");
     }
 }
